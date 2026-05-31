a02a80e3937665be9c4fa30753e52c12f544d2cf
Sajjad Ali <yoursajju61@gmail.com>
2026-05-31 14:31:13 +0530

    feat: Add comments explain code to reader, Problem 131

--- 131-remove-givenval-list/remove_value_list.py
+++ 131-remove-givenval-list/remove_value_list.py
@@ -1,20 +1,21 @@
 # WAP to read n values into list remove given value from list
 
-list1 = []
+list1 = []  # Empty List
 
-n = int(input("Enter the number of values to be read : "))
+n = int(input("Enter the number of values to be read : "))  # User input / length of the list
 
 for i in range(n):
-    value = int(input("Enter the value : "))
-    list1.append(value)
+    value = int(input("Enter Elements : "))  # list elements
+    list1.append(value)  # adding elements to list1
 
-print(f'Before Deleting value : {list1}')
+print(f'Before Deleting value : {list1}')  # printing the OG list
 
-value = int(input("Enter the value to be deleted : "))
+value = int(input("Enter the value you wanna delete : "))  # del element
 
-if value in list1:
-    i = list1.index(value)
+if value in list1:  # CHECK : Element exist or not 
+    i = list1.index(value)  # Getting Location of element
+    #print(i)  # FOR CHECKING PURPOSE index show
     del list1[i]
     
-    print(f'{value} is deleting from list')
-    print(f'After Deleting value : {list1}')
+    print(f'{value} is deleting from list')  #  Deleting print
+    print(f'After Deleting value : {list1}')  # List1 printing after deleting element deleted
