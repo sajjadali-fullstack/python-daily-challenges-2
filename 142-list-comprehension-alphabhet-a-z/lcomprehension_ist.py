8e177d4be5f93b3607d55e553c7e9f895d681225
Sajjad Ali <yoursajju61@gmail.com>
2026-06-02 14:28:03 +0530

    feat: List comprehension with Alphabets from a-z, Problem 142

---
+++ 142-list-comprehension-alphabhet-a-z/lcomprehension_ist.py
@@ -0,0 +1,7 @@
+# WAP List comprehension with Alphabets from a-z
+
+print(ord('a'))  # 97
+print(ord('z'))  # 122
+
+list1 = [chr(i) for i in range(97, 123)]
+print(list1)  # ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
