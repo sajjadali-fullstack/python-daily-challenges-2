030d99d41c5f5527c0991557a3b94c30af1c9889
Sajjad Ali <yoursajju61@gmail.com>
2026-06-04 22:58:35 +0530

    feat: Assignment-1 PRINT EMENT Q's wise. [QST] , Problem 152

---
+++ 152-Assignment-2/print_element.py
@@ -0,0 +1,14 @@
+# Q.2 PRINT
+# (1) ALL ELEMENTS IN THE LIST,
+# (2) 'CHANEL' 'MITCH' 'RYAN',
+# (3) FIRST THREE ELEMENTS,
+# (4) LAST THREE ELEMENTS,
+# (5) SKIP EACH OTHER ELEMENT
+
+name = ["sara", "chanel", "mitch", "ryan", "holy", "alex", "rob"]
+
+print(f'1. All Elements : {name}')
+print(f'2. {name[1:4]}')
+print(f'3. {name[0:3]}')
+print(f'4. {name[4:]}')
+print(f'5. {name[0::2]}')
