058f7d75c50954069148a4e314d23d9357144674
Sajjad Ali <yoursajju61@gmail.com>
2026-06-04 22:46:25 +0530

    feat: Assignment-1 PRINT FIRST, SECOND & LAST ELEMENT IN THE LIST. [QST] , Problem 151

---
+++ 151-Assignment-1/first_sec_last.py
@@ -0,0 +1,8 @@
+# Q.1 PRINT FIRST, SECOND & LAST ELEMENT IN THE LIST. [QST] 
+
+name = ["sara", "chanel", "mike", "ryan", "holy", "alex", "rob"]
+
+elements = name[0:2]
+elements.append('rob')
+
+print(elements)
