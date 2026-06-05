c3abc324f7d8aed12fc1b6ee889c164d3c39b105
Sajjad Ali <yoursajju61@gmail.com>
2026-06-05 18:57:08 +0530

    feat: Assignment-6 DISPLAY ELEMENTS Q's wise. [QST] , Problem 156

---
+++ 156-Assignment-6/display_accordingly.py
@@ -0,0 +1,9 @@
+# Q.6 SORT ELEMENT IN A LIST & REVERSE ALL ELEMENTS TO GO FROM MAX TO MIN
+# -sort(), reverse()
+
+list_num = [10, 40, 20, 30, 50]
+
+list_num.sort()
+list_num.reverse()
+
+print(list_num)
