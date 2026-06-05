850dd2d622ac7a4d1d82f2d48d4c94ee0ff76783
Sajjad Ali <yoursajju61@gmail.com>
2026-06-05 18:35:08 +0530

    feat: Assignment-3 DISPLAY ELEMENTS Q's wise. [QST] , Problem 153

---
+++ 153-Assignment-3/display_accordingly.py
@@ -0,0 +1,8 @@
+# Q.3 PRINT (1) SALLY (2) SARA, SALLY, JOE (3) ALY (WITH TWO DIFFERENT WAYS)
+
+my_list = ["Mitch", ["sara", "sally", "joe"], ["peter", "aly"]]
+
+print(f'1. {my_list[1][1]}')
+print(f'2. {my_list[1][0:]}')
+print(f'3. {my_list[2][-1]}')
+print(f'3. {my_list[2][1]}')
