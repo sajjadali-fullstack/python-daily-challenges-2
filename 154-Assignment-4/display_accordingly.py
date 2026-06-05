c6db38a9feadec4d39c87fc718aab9643a398a87
Sajjad Ali <yoursajju61@gmail.com>
2026-06-05 18:42:23 +0530

    feat: Assignment-4 DISPLAY ELEMENTS Q's wise. [QST] , Problem 154

---
+++ 154-Assignment-4/display_accordingly.py
@@ -0,0 +1,10 @@
+# Q.4 PRINT THE LENGTH OF THE LIST & ADD YOUR NAME TO THE LIST (EXTERNAL RESEARCH)
+# - Research the method append():
+
+my_list = ["Mitch", ["sara", "sally", "joe"], ["peter", "aly"]]
+
+length = len(f'Length of the list : {my_list}')
+print(length)
+
+my_list.append("Sajjad ali")
+print(f'Updated list : {my_list}')
