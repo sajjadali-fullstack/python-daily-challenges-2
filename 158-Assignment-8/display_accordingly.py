80eb8cb8ff6be23926959c649f97391180eb86f4
Sajjad Ali <yoursajju61@gmail.com>
2026-06-05 19:21:03 +0530

    feat: Assignment-8 DISPLAY ELEMENTS Q's wise. [QST] , Problem 158

---
+++ 158-Assignment-8/display_accordingly.py
@@ -0,0 +1,7 @@
+# Q.8 CREATE A LIST WITH 5 OF YOUR CLOSEST FRIENDS & SORT THEM ALPHABETICALLY [QST]
+
+my_friends = ["Ali", "Sana", "Rahim", "Pooja", "Aleem", "Faisal"]
+print(f'Original List : {my_friends}')
+
+my_friends.sort()
+print(f'After Sorting : {my_friends}')
