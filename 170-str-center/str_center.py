6b25043059e6883bc90314a4b2de2a6e8b4598c6
Sajjad Ali <yoursajju61@gmail.com>
2026-06-24 23:23:12 +0530

    feat: Add string center() method example, Problem 170

---
+++ 170-str-center/str_center.py
@@ -0,0 +1,7 @@
+# WAP to center-align a string within a specified width using a custom fill character.
+
+str1 = "ali"
+s2 = str1.center(9, "-")
+
+print(f'Original Str : {str1}')
+print(f'Center Str : {s2}')
