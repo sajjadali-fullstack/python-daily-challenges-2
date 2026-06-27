eb6e9b69c99146604b621aeb944b083b866a1347
Sajjad Ali <yoursajju61@gmail.com>
2026-06-27 22:16:37 +0530

    feat: Add tuple length and type example, Problem 174

---
+++ 174-Assignment-11/tuple_length.py
@@ -0,0 +1,17 @@
+# WAP to find the length and data type of a tuple in Python. [QST]
+'''
+Write a Python program that creates a tuple containing different data types and displays:
+
+1. The length of the tuple.
+2. The data type of the tuple.
+3. Both values using a formatted output.
+'''
+
+
+my_tuple = ("Sajjad", "Ali", 20)
+
+print(f'Length of tuple is : {len(my_tuple)}')
+
+print(f'Data type of tuple is : {type(my_tuple)}')
+
+print("Length of an element is:", len(my_tuple),"\nType of an element is:", type(my_tuple))
