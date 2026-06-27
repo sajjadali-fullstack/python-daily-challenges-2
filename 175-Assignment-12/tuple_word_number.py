db81029541b5d1bf9d840c6d1d2cce8b428238a6
Sajjad Ali <yoursajju61@gmail.com>
2026-06-27 22:23:41 +0530

    feat: Revise Python nested tuple indexing, Problem 174

---
+++ 175-Assignment-12/tuple_word_number.py
@@ -0,0 +1,10 @@
+# Q.4 (1)PRINT THE NUMBER '40"
+# (2) PRINT THE WORD "WORLD",
+# (3) PRINT THE NUMBER 25 & PRINT THE ENTIRE LIST & TUPLE LISTED BELOW [QST]
+
+my_tuple = ("hello", [87, 40, 60], "world", (10, 25, 30))
+
+print(my_tuple[1][1])  # 40
+print(my_tuple[2][0:])
+print(my_tuple[3][1])
+print(my_tuple)
