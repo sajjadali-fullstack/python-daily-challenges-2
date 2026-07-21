0f5a4a13b125c455e8acd0239cff519ebe418e4c
Sajjad Ali <yoursajju61@gmail.com>
2026-07-21 14:01:31 +0530

    feat: strip

---
+++ 179-strip/strip.py
@@ -0,0 +1,9 @@
+# Write a Python program to remove the characters w, c, o, m, and . 
+# from both ends of the given string using the strip() method.
+
+
+name = "www.helloworld.com"
+print(name)
+
+st = name.strip('wcom.')
+print(st)
