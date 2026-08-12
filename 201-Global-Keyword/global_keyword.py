2801163f31e51169a826fcd77d34883f0c7b7137
Sajjad Ali <yoursajju61@gmail.com>
2026-08-12 17:24:15 +0530

    practice: revise global keyword

--- 201-Global-Keyword/global_keyword.py
+++ 201-Global-Keyword/global_keyword.py
@@ -1,1 +1,25 @@
 # to demonstrate the use of the global keyword to modify a global variable inside a function.
+
+
+# global keyword
+
+a = 100  # Global Variable
+
+
+def fun1():
+    print(a)  # Accessing Global Variable
+
+
+def fun2():
+    global a  # Using Global Keyword
+    a = 500   # Modifying Global Variable
+    print(a)
+
+
+def fun3():
+    print(a)  # Accessing Modified Global Variable
+
+
+fun1()  # Function Call
+fun2()  # Function Call
+fun3()  # Function Call
