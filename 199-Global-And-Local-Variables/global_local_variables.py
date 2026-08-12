c50433873e0d7b0ab3b2b7e9bda22495579cb125
Sajjad Ali <yoursajju61@gmail.com>
2026-08-12 16:46:09 +0530

    practice: revise global and local variables

---
+++ 199-Global-And-Local-Variables/global_local_variables.py
@@ -0,0 +1,13 @@
+
+#  WAP to demonstrate the use of global and local variables inside a function.
+# Global Variables
+
+num1 = 1500  # Global Variables
+num2 = 4500  # Global Variables
+
+
+def add():
+    print(f'sum of {num1} and {num2} is {num1 + num2}')
+
+
+add()  # Function Call / Executable Statement
