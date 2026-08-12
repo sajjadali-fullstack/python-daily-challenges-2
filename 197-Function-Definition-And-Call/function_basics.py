2f291399b850e42b7f25285769d4c4bcce9db4e3
Sajjad Ali <yoursajju61@gmail.com>
2026-08-12 16:01:38 +0530

    practice: revise function definition and function call

---
+++ 197-Function-Definition-And-Call/function_basics.py
@@ -0,0 +1,19 @@
+# WAP to define a function draw_line() that prints 15 asterisks (*) and call the function multiple times to display separator lines.
+
+
+# Function Definition
+def draw_line():
+    for i in range(25):
+        print('*', end='')
+    print()
+
+
+draw_line()  # Executable statement / Function Call
+
+print("Python")  # Executable statement / Function Call
+
+draw_line()  # Executable statement / Function Call
+
+print("Programming Language")  # Executable statement
+
+draw_line()  # Executable statement / Function Call
