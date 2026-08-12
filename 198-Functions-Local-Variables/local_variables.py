023208f1144677d7618835838c4280e0ac304ed2
Sajjad Ali <yoursajju61@gmail.com>
2026-08-12 16:21:50 +0530

    practice: revise local variables and function parameters

---
+++ 198-Functions-Local-Variables/local_variables.py
@@ -0,0 +1,12 @@
+# WAP to define a function add() that accepts two numbers as parameters, calculates their sum using a local variable, and displays the result.
+
+
+
+def add():  # function Definition
+    n1 = int(input("Enter 1 Number :"))  # Local variable
+    n2 = int(input("Enter 1 Number :"))  # Local variable
+    t = n1 + n2  # Local variable
+    print(f'Sum of {n1} and {n2} is {t}')
+
+
+add()  # Function Call / Executable Statement
