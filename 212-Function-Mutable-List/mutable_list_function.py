ec1d9224a19ba10b696aa4fd5fd2c03d7547cf65
Sajjad Ali <yoursajju61@gmail.com>
2026-08-13 15:26:24 +0530

    practice: revise mutable list with function arguments

---
+++ 212-Function-Mutable-List/mutable_list_function.py
@@ -0,0 +1,21 @@
+
+
+
+
+def fun1(a):  # required type
+    a[0] = 99
+    a.append(100)
+
+
+def fun2():
+    lst1 = [10, 20, 30]
+    print(f'Before Calling Function : {lst1}')
+    fun1(lst1)  # object reference is passed to the function.
+    print(f'After Calling Function : {lst1}')
+
+
+fun2()  # calling
+
+
+# Before Calling Function : [10, 20, 30]
+# After Calling Function : [99, 20, 30, 100]
